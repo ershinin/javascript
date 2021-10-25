14476a7f9b98a63f04f4e4da704294051f353be2
nvershinin <vershinin_nikita@mail.ru>
2021-10-25 18:57:38 +0300

    updated

--- homework_3/js/script.js
+++ homework_3/js/script.js
@@ -72,7 +72,7 @@ console.log("Логин найден");
 
 console.log("Задание 5.");
 logins = ["Xodahe", "Gixath", "Qexeme", "Bolyky", "Fifiwu"];
-tryLogin = prompt("Введите логин");
+tryLogin = prompt("Придумайте логин");
 while (logins.includes(tryLogin)) {
     tryLogin = prompt("Логин уже используется. Попробуйте придумать другой");
 }
